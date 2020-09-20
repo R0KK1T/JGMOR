dd9449c7cd07ab4c16a7d1ff38d97125c821fd26
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-09-20 13:41:03 +0200

    Fixed a bug with regtangle intersection and wrote tests for Rectangle and Frog

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Rectangle.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Rectangle.java
@@ -13,8 +13,8 @@ public class Rectangle {
         this.height = height;
     }
     public boolean intersect(Rectangle rect){
-        return !((this.x > rect.getX()+rect.getWidth()) || (this.x + this.width < rect.getWidth())
-                || (this.y > rect.getY() + rect.getHeight()) || (this.y + this.height < rect.getY()));
+        return !((this.x >= rect.getX()+rect.getWidth()) || (this.x + this.width <= rect.getWidth())
+                || (this.y >= rect.getY() + rect.getHeight()) || (this.y + this.height <= rect.getY()));
     }
 
     public int getHeight() {
--- src/test/java/edu/chalmers/projecttemplate/model/ProjectTest.java
+++ src/test/java/edu/chalmers/projecttemplate/model/ProjectTest.java
@@ -1,8 +1,30 @@
 package edu.chalmers.projecttemplate.model;
 
+import edu.chalmers.projecttemplate.model.froggermodel.Frog;
+import edu.chalmers.projecttemplate.model.froggermodel.Rectangle;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ProjectTest {
-
+    @Test
+    public void intersectTest(){
+        Rectangle rect1 = new Rectangle(10,10,10,10);
+        Rectangle rect2 = new Rectangle(10,20,10,10);
+        Assert.assertFalse(rect1.intersect(rect2));
+        Rectangle rect3 = new Rectangle(10,10,10,10);
+        Rectangle rect4 = new Rectangle(15,10,10,10);
+        Assert.assertTrue(rect3.intersect(rect4));
+    }
+    @Test
+    public void moveFrog(){
+        Frog frog = new Frog(10, 10, 10, 10);
+        frog.moveRight();
+        Assert.assertTrue(frog.getHitbox().getX() == 11);
+        frog.moveUp();
+        Assert.assertTrue(frog.getHitbox().getY() == 9);
+        frog.moveLeft();
+        Assert.assertTrue(frog.getHitbox().getX() == 10);
+        frog.moveDown();
+        Assert.assertTrue(frog.getHitbox().getY() == 10);
+    }
 }
