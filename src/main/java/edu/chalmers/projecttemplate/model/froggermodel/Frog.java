19434d9a96d3fe27c8f7c4c440d754fa5ba58f69
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-09-20 12:58:19 +0200

    Created properties for rectangle and added hitbox to frog

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Frog.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Frog.java
@@ -1,4 +1,12 @@
 package edu.chalmers.projecttemplate.model.froggermodel;
 
 public class Frog {
+    private Rectangle hitbox;
+    public Frog(int x, int y, int width, int height){
+        hitbox = new Rectangle(x, y, width, height);
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
 }
--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Rectangle.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Rectangle.java
@@ -1,4 +1,35 @@
 package edu.chalmers.projecttemplate.model.froggermodel;
 
 public class Rectangle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public boolean intersect(Rectangle rect){
+        return !((this.x > rect.getX()+rect.getWidth()) || (this.x + this.width < rect.getWidth())
+                || (this.y > rect.getY() + rect.getHeight()) || (this.y + this.height < rect.getY()));
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
