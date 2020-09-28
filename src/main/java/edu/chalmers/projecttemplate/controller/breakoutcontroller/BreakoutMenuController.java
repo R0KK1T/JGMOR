d564496e88bdfc4a71ff73e74b47582a77dec7e8
Georges Kayembe <kayss.g@gmail.com>
2020-09-28 20:28:01 +0200

    Changed Breakout -Menu -Buttons

--- src/main/java/edu/chalmers/projecttemplate/controller/breakoutcontroller/BreakoutMenuController.java
+++ src/main/java/edu/chalmers/projecttemplate/controller/breakoutcontroller/BreakoutMenuController.java
@@ -1,15 +1,14 @@
 package edu.chalmers.projecttemplate.controller.breakoutcontroller;
 
 
-import javafx.event.EventHandler;
+import edu.chalmers.projecttemplate.model.breakoutmodel.BreakoutButtonModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 
-public class BreakoutMenuController  {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BreakoutMenuController extends BreakoutButtonModel  {
     @FXML
     public Button btnPlay;
     @FXML
@@ -18,21 +17,56 @@ public class BreakoutMenuController  {
     public Button btnHelp;
     @FXML
     public Button btnExit;
+    BreakoutButtonModel breakoutButtonModel;
+    List<Button> buttonList;
     //constructor
     public BreakoutMenuController() { }
     public void initialize() {
         // handle data once the fields are injected
         controlPlay();
+        controlScores();
+        controlHelp();
+        controlExit();
+        breakoutButtonModel = new BreakoutButtonModel();
+        buttonList = new ArrayList<>();
+        addToButtoList();
+        buttonsettings();
     }
     //1. Button play
     private void controlPlay() {
-        btnPlay.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-
-            }
-        });
+        btnPlay.setOnMouseClicked(event ->
+                System.out.println("Play"));
 
     }
+    //2. Button score
+    private void controlScores() {
+        btnScores.setOnMouseClicked(event ->
+                System.out.println("Scores"));
+    }
+    //3. Button score
+    private void controlHelp() {
+        btnHelp.setOnMouseClicked(event ->
+                System.out.println("Help"));
+    }
+    //4. Button exit
+    private void controlExit() {
+        btnExit.setOnMouseClicked(event -> System.exit(0));
+
+    }
+    //Settings
+    private void addToButtoList() {
+        buttonList.add(btnPlay);
+        buttonList.add(btnScores);
+        buttonList.add(btnHelp);
+        buttonList.add(btnExit);
+    }
+    private void buttonsettings() {
+        //Add font, set cursor
+        for (int i=0; i<4; i++) {
+            breakoutButtonModel.setButtonFront(buttonList.get(i));
+            breakoutButtonModel.setCursor(buttonList.get(i));
+        }
+    }
+
 
 }
