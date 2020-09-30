0c7fe54495cd0dab1d4bf58c364936286aee2fcc
MhdJamal <45226938+MhdJamal@users.noreply.github.com>
2020-09-30 13:08:53 +0200

    Create Game Window

--- src/main/java/edu/chalmers/projecttemplate/controller/snakecontroller/SnakeController.java
+++ src/main/java/edu/chalmers/projecttemplate/controller/snakecontroller/SnakeController.java
@@ -1,4 +1,82 @@
 package edu.chalmers.projecttemplate.controller.snakecontroller;
 
+import edu.chalmers.projecttemplate.model.snakemodel.Snake;
+
 public class SnakeController {
+
+
+    private boolean started = false;
+    private boolean finished = false;
+    private int score = 0;
+    private int seconds = 0;
+    private boolean pause = false;
+    private boolean startMenu = true;
+
+    private String menuSelection = "Start game";
+
+    private Snake snake;
+    private SnakeController snakeGame;
+
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setSnakeGame(SnakeController snakeGame) {
+        this.snakeGame = snakeGame;
+    }
+
+    public boolean isPause() {
+        return pause;
+    }
+
+
+    public void setPause(boolean pause) {
+        this.pause = pause;
+    }
+
+
+    public String getMenuSelection() {
+        return menuSelection;
+    }
+
+
+    public void setMenuSelection(String menuSelection) {
+        this.menuSelection = menuSelection;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+
+    public boolean isStartmenu() {
+        return startMenu;
+    }
+
 }
--- src/main/java/edu/chalmers/projecttemplate/model/snakemodel/GUI.java
+++ src/main/java/edu/chalmers/projecttemplate/model/snakemodel/GUI.java
@@ -1,5 +1,6 @@
 package edu.chalmers.projecttemplate.model.snakemodel;
 
+
 import edu.chalmers.projecttemplate.controller.snakecontroller.SnakeController;
 
 import java.awt.*;
@@ -12,9 +13,17 @@ import javax.swing.*;
 public class GUI {
 
     public static JFrame f1;
+    public static JPanel p1;
+
+
+
+    private SnakeController snakeGame;
+    private Font fontMenu;
+    private Font fontHeader;
 
     public GUI() {
-
+        snakeGame = new SnakeController();
+        snakeGame.setSnakeGame(snakeGame);
     }
 
 
@@ -31,7 +40,110 @@ public class GUI {
             e.printStackTrace();
         }
 
+        f1.setLocationRelativeTo(null);
+        p1 = new Draw();
+        p1.setLayout(null);
+        f1.add(p1);
 
+        //Create Fonts
+        fontMenu = new Font( Font.SANS_SERIF, Font.PLAIN, 12 );
+        fontHeader = new Font( Font.SANS_SERIF, Font.BOLD, 16 );
+
+        f1.requestFocus();
+        f1.repaint();
+        f1.setVisible(true);
+
+        f1.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(snakeGame.isStarted()) {
+
+                    //Pause Menu
+                    if(snakeGame.isPause()){
+
+                        checkPauseMenuInputs(e);
+
+                        //Current Game
+                    }else if(!snakeGame.isPause()) {
+
+
+                    }
+
+                    //Game Over Menu
+                }else if(snakeGame.isFinished()) {
+
+
+                    //Start Menu
+                }else if(snakeGame.isStartmenu()) {
+
+
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+
+        });
+
+    }
+
+
+    class Draw extends JPanel {
+
+        private static final long serialVersionUID = 1L;
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+
+            if (snakeGame.isStarted()) {
+
+                 // Draw start menu
+            }
+
+        }
+    }
+
+
+    // -----------KeyListener-Methods---------------------
+
+    public void checkPauseMenuInputs(KeyEvent e) {
+        //ESC
+        if(e.getKeyCode()==27) {
+            snakeGame.setPause(false);
+            //Up
+        }else if(e.getKeyCode()==38) {
+            if(snakeGame.getMenuSelection().equals("Exit game")) {
+                snakeGame.setMenuSelection("Back to main menu");
+                GUI.f1.repaint();
+            }else if(snakeGame.getMenuSelection().equals("Back to main menu")) {
+                snakeGame.setMenuSelection("Restart game");
+                GUI.f1.repaint();
+            }
+            //Down
+        }else if(e.getKeyCode()==40) {
+            if(snakeGame.getMenuSelection().equals("Restart game")) {
+                snakeGame.setMenuSelection("Back to main menu");
+                GUI.f1.repaint();
+            }else if(snakeGame.getMenuSelection().equals("Back to main menu")) {
+                snakeGame.setMenuSelection("Exit game");
+                GUI.f1.repaint();
+            }
+
+        }
+    }
+
+
+    public void setSnakeGame(SnakeController snakeGame) {
+        this.snakeGame = snakeGame;
     }
 }
 
