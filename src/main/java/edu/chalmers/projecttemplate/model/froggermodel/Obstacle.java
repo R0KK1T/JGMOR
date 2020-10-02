4a58206b22d46c864b3cf914053bbbb901115f5c
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-10-02 12:00:36 +0200

    Added basic functionality to Obstacle class

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Obstacle.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Obstacle.java
@@ -1,4 +1,34 @@
 package edu.chalmers.projecttemplate.model.froggermodel;
 
-public class Obstacle {
+import edu.chalmers.projecttemplate.model.common.Rectangle;
+
+public abstract class Obstacle {
+    private Rectangle hitbox;
+
+    public Obstacle(int x, int y, int width, int height){
+        hitbox = new Rectangle(x, y, width, height);
+    }
+    
+    //Very much alike Frog. Might be smart to refactor with superclass or other
+    //In order to minimize duplication of code.
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+    public int getX(){ return getHitbox().getX(); }
+    public int getY(){ return getHitbox().getY(); }
+    public int getWidth(){ return getHitbox().getWidth(); }
+    public int getHeight(){ return getHitbox().getHeight(); }
+
+    public void moveRight(){
+        hitbox.incX();
+    }
+    public void moveLeft(){
+        hitbox.decX();
+    }
+    public void moveUp(){
+        hitbox.decY();
+    }
+    public void moveDown(){
+        hitbox.incY();
+    }
 }
