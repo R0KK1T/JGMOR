c7ed316bb74294070b37f4c4c164ed40610d90ba
MhdJamal <45226938+MhdJamal@users.noreply.github.com>
2020-10-04 11:59:26 +0200

    adjustment of food class

--- src/main/java/edu/chalmers/projecttemplate/model/snakemodel/Food.java
+++ src/main/java/edu/chalmers/projecttemplate/model/snakemodel/Food.java
@@ -1,36 +1,20 @@
 package edu.chalmers.projecttemplate.model.snakemodel;
 
+import java.util.List;
+import java.util.Random;
+
 public class Food {
 
-    private int foodX = 0;
-    private int foodY = 0;
-    private boolean foodPlaced = false;
-    private Food food;
+    private int foodPositionX;
+    private int foodPositionY;
 
+    Random random = new Random();
 
-    public int getFoodX() {
-        return foodX;
-    }
-    public void setFoodX(int essenX) {
-        this.foodX = essenX;
-    }
-    public int getFoodY() {
-        return foodY;
-    }
-    public void setFoodY(int essenY) {
-        this.foodY = essenY;
-    }
-    public boolean isFoodPlaced() {
-        return foodPlaced;
-    }
-    public void setFoodPlaced(boolean foodPlaced) {
-        this.foodPlaced = foodPlaced;
-    }
-    public Food getFood() {
-        return food;
-    }
-    public void setFood(Food food) {
-        this.food = food;
+    public void  generatePosition(){
+        //Food can be generated on top of Snake's position.
     }
 
+    public int getFoodPositionX(){return foodPositionX;}
+
+    public int getFoodPositionY(){return foodPositionY;}
 }
