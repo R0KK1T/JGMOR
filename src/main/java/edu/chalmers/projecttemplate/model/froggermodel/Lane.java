e33435abc92486d9420ae91e7e65616a6908f44d
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-10-05 11:08:30 +0200

    Added LaneFactory and some basic characteristics for class Lane

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Lane.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Lane.java
@@ -1,4 +1,13 @@
 package edu.chalmers.projecttemplate.model.froggermodel;
 
+import java.util.ArrayList;
+
 public class Lane {
+    private ArrayList<Obstacle> obstacles;
+    private int velocity;
+    private boolean river;
+    //Plan is to utilize a factory pattern to generate Lanes
+    public Lane(){
+
+    }
 }
