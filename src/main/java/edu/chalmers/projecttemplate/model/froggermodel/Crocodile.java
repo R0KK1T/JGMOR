e76b971882094d9ea38167b2ff389168df8b37bf
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-10-07 21:29:15 +0200

    Removed unnecessary classes

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/Crocodile.java
+++
@@ -1,7 +0,0 @@
-package edu.chalmers.projecttemplate.model.froggermodel;
-
-public class Crocodile extends Obstacle {
-    public Crocodile(int x, int y, int width, int height, int velocity) {
-        super(x, y, width, height, velocity);
-    }
-}
