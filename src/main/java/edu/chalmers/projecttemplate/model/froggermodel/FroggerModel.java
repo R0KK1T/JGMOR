b9cad3bb8559a80dbc3d673f60935ce525ff1dc5
Jonathan Stigson <jonathan.stigson@gmail.com>
2020-10-09 15:59:45 +0200

    Initial commit for class FroggerModel, just a few attributes

--- src/main/java/edu/chalmers/projecttemplate/model/froggermodel/FroggerModel.java
+++ src/main/java/edu/chalmers/projecttemplate/model/froggermodel/FroggerModel.java
@@ -1,4 +1,14 @@
 package edu.chalmers.projecttemplate.model.froggermodel;
 
+import java.util.ArrayList;
+
 public class FroggerModel {
+    private int squareDimension;
+    private int windowSizeX;
+    private int windowSizeY;
+
+    private Frog player;
+    private LaneFactory factory;
+    private ArrayList<Lane> lanes;
+
 }
