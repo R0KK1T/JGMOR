04aa8730866d7eccb8dc0db20f654417836af4c6
Robert Sahlqvist <robert.sahlqvist@hotmail.com>
2020-10-20 17:38:11 +0200

    Added some empty test methods as a start

---
+++ src/test/java/edu/chalmers/projecttemplate/model/PongTest.java
@@ -0,0 +1,17 @@
+package edu.chalmers.projecttemplate.model;
+
+import org.junit.Test;
+
+import java.security.PublicKey;
+
+public class PongTest {
+    @Test
+    public void PaddleUpdateTest()
+    {
+
+    }
+    @Test
+    public void PaddleMoveTest(){
+        
+    }
+}
